b30f20b95855db19c2f27f2f9a4c4a1187c83b8a
iLem0n <peter.c.glade@googlemail.com>
2015-12-16 16:33:50 +0100

    .

--- fotographer/accept-job.php
+++ fotographer/accept-job.php
@@ -4,9 +4,9 @@ require_once('../includes/db-connect.php');
 
 	try {
 		$dbMgr = new DBManager();
-		$acceptJob($_GET[job_id], $_GET[user_id]);
+		acceptJob($_GET[job_id], $_GET[user_id]);
 	} catch (Exception $e) {
 		echo getFeedback($e->getMessage(),'danger');
 	}
-	
+
 ?>
