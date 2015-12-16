dc78842a19d77b9b58f88f0912ed8252bd3fb517
iLem0n <peter.c.glade@googlemail.com>
2015-12-16 15:35:18 +0100

    .

--- fotographer/joblist.php
+++ fotographer/joblist.php
@@ -4,12 +4,31 @@ require_once('../includes/functions.php');
 require_once('../includes/db-connect.php');
 
  /* $result = mysql_query("SELECT * FROM articles WHERE id = $id"); */
- $user_id = $_SESSION['user_id'];
-$statement = $conn->prepare("SELECT * FROM jobs WHERE job_fotographer_id = '$user_id'");
-$statement->execute(array(
-':id' => $id
-  ));
- while ( $job = $statement->fetch(PDO::FETCH_OBJ) ) { ?>
+
+try {
+  $user_id = $_SESSION['user_id'];
+  $jobsStatement = $conn->prepare("SELECT * FROM jobs WHERE job_fotographer_id = '$user_id'");
+  $locationsStatement = $conn->prepare("SELECT * FROM locations WHERE loc_job_id = :job_id");
+
+  $jobsStatement->execute(array(
+    ':user_id' => $user_id
+    ));
+
+} catch(PDOException $e) {
+  feedback = getFeedback($e->getMessage());
+}
+
+while ( $job = $jobsStatement->fetch(PDO::FETCH_OBJ) ) { 
+    try {
+      $locationsStatement->execute(array(
+      ':job_id' => $job->job_id
+        ));
+    
+    } catch(PDOException $e) {
+      $feedback = getFeedback($e->getMessage());
+    }
+
+    ?>
    <article>
           <header>
             <h3><?= $job->job_title; ?></h3>
@@ -17,11 +36,12 @@ $statement->execute(array(
           <div class="article-teaser">
       
             <p>
-              Beschreibung            
+            Locations: <?= $locationsStatement->rowCount() ?> 
             </p>
           </div>
           <footer>
-            <a href="<?= "index.php" ?>" class="btn btn-primary"></a>
+            <a href="<?= "index.php" ?>" class="btn btn-primary">Accept</a>
+            <a href="<?= "index.php" ?>" class="btn btn-info">Details</a>
           </footer>
         </article> 
         <?php
