9cb35afb02b0448d36aeaa4a88d12a8a434e39f2
iLem0n <peter.c.glade@googlemail.com>
2015-12-16 14:50:13 +0100

    Update joblist.php

--- fotographer/joblist.php
+++ fotographer/joblist.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once('/stov/includes/functions.php');
-require_once('/stov/includes/db-connect.php');
+require_once('../../functions.php');
+require_once('../../db-connect.php');
 
  /* $result = mysql_query("SELECT * FROM articles WHERE id = $id"); */
  $user_id = $_SESSION['user_id'];
