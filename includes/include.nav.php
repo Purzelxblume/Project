e91350a7acd52eefb3d0ef97d503d7f11aa71baf
iLem0n <peter.c.glade@googlemail.com>
2015-12-16 13:57:18 +0100

    Update include.nav.php

--- includes/include.nav.php
+++ includes/include.nav.php
@@ -16,13 +16,6 @@
     	<li role="presentation"><a href="/stov/logout.php">Log out</a></li>
 		<?php
     }
-    else
-    {
     	?>
-    	<li role="presentation"><a href="login.php">Login</a></li>
-    	<?php
-    }
-
-    ?>
   </ul>
 </nav>
