4f20cf562bce2866220bb1042b1844ec6aa89820
iLem0n <peter.c.glade@googlemail.com>
2015-12-16 14:12:34 +0100

    Update include.nav.php

--- includes/include.nav.php
+++ includes/include.nav.php
@@ -2,14 +2,14 @@
   <ul class="nav nav-pills">
     <?php
 
-    if ( hasRights(100) )
+    if ( getRights() == 100 )
     {
     	?>
     	<li role="presentation"><a href="/stov/fotographer/index.php">Home</a></li>
     	<li role="presentation"><a href="/stov/logout.php">Log out</a></li>
 		<?php
     }
-    elseif( hasRights(101) ){
+    elseif( getRights() == 101 ){
     	?>
     	<li role="presentation"><a href="/stov/customer/index.php">Home</a></li>
     	<li role="presentation"><a href="/stov/logout.php">Log out</a></li>
